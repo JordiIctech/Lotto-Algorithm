75b37aa632b1727ac7da621da6c8046488855e35
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2023-01-06 13:51:08 -0500

    Session created

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import numpy as np
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.context import SparkContext
+
+Spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+df = Spark.read.csv("Data/Lottery_Mega_Millions_Winning_Numbers__Beginning_2002.csv")
+
+df.show()
